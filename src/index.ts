cebf36f303f1f51405675b9764f773e4fd2e1a8a
Erik Olson <erikamerico1991@gmail.com>
2025-06-14 01:14:27 -0400

    remove rate-limit dep

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
-import rateLimit from "express-rate-limit";
 import playersRouter from "./routes/players";
-
+import matchesRouter from "./routes/matches";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
-app.use("/players", playersRouter);
-const port = process.env.PORT || 3000;
 
 // app.use(cors({ origin: "https://foosball.life" })); ////for prod
 app.use(cors()); ////for dev
+app.use(express.json());
+app.use(helmet());
+
+const port = process.env.PORT || 3000;
+
+app.use("/players", playersRouter);
+app.use("/matches", matchesRouter);
 
 app.listen(port, () => {
   console.log(`🚀 Server running at http://localhost:${port}`);
