b4a4a6c91617600a49e8b8557cf9a20d59d2f92d
Erik Olson <erikamerico1991@gmail.com>
2025-06-23 03:12:51 -0400

    create tournament seed
    from real game results

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,3 @@
-// prisma/seed.ts
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -45,6 +44,145 @@ async function main() {
   });
 
   console.log(`🌱 Seeded ${players.length} players`);
+
+  await prisma.tournament.deleteMany({});
+  console.log("tournaments removed");
+
+  //may 18 2024
+  const tournaments = [
+    {
+      teams: [
+        [
+          { id: "cmc5n3zxp0008p5lo2vk2vee8", name: "Tommy B", score: 11 },
+          { id: "cmc5n3zxp0004p5lopldq6jnd", name: "Dan F", score: 10.5 },
+        ],
+        [
+          { id: "cmc5n3zxp0001p5lob5eewb2q", name: "Ofir C", score: 31.25 },
+          { id: "cmc5n3zxp000fp5lon3fv26zk", name: "Andraya B", score: 17.5 },
+        ],
+        [
+          {
+            id: "cmc5n3zxp0005p5loaf1n99a9",
+            name: "Rachelle J",
+            score: 9.75,
+          },
+          { id: "cmc5n3zxp000ip5loedokujy3", name: "Ari S", score: 6.5 },
+        ],
+        [
+          { id: "cmc5n3zxp0000p5loow0ddrlx", name: "Erik O", score: 28.25 },
+          { id: "cmc5n3zxp0002p5lo56189qbh", name: "Anna O", score: 18.5 },
+        ],
+        [
+          { id: "cmc5n3zxp000gp5lo5fmsmk57", name: "Brickwall", score: 14.5 },
+          { id: "cmc5n3zxp0003p5lo6s66t7yk", name: "Michelle C", score: 23 },
+        ],
+      ],
+      results: [
+        { winner: null, loser: null },
+        {
+          winner: [
+            { id: "cmc5n3zxp000gp5lo5fmsmk57", name: "Brickwall", score: 14.5 },
+            { id: "cmc5n3zxp0003p5lo6s66t7yk", name: "Michelle C", score: 23 },
+          ],
+          loser: [
+            { id: "cmc5n3zxp0000p5loow0ddrlx", name: "Erik O", score: 28.25 },
+            { id: "cmc5n3zxp0002p5lo56189qbh", name: "Anna O", score: 18.5 },
+          ],
+        },
+        {
+          winner: [
+            { id: "cmc5n3zxp0001p5lob5eewb2q", name: "Ofir C", score: 31.25 },
+            { id: "cmc5n3zxp000fp5lon3fv26zk", name: "Andraya B", score: 17.5 },
+          ],
+          loser: [
+            {
+              id: "cmc5n3zxp0005p5loaf1n99a9",
+              name: "Rachelle J",
+              score: 9.75,
+            },
+            { id: "cmc5n3zxp000ip5loedokujy3", name: "Ari S", score: 6.5 },
+          ],
+        },
+        {
+          winner: [
+            { id: "cmc5n3zxp000gp5lo5fmsmk57", name: "Brickwall", score: 14.5 },
+            { id: "cmc5n3zxp0003p5lo6s66t7yk", name: "Michelle C", score: 23 },
+          ],
+          loser: [
+            { id: "cmc5n3zxp0008p5lo2vk2vee8", name: "Tommy B", score: 11 },
+            { id: "cmc5n3zxp0004p5lopldq6jnd", name: "Dan F", score: 10.5 },
+          ],
+        },
+        {
+          winner: [
+            { id: "cmc5n3zxp0000p5loow0ddrlx", name: "Erik O", score: 28.25 },
+            { id: "cmc5n3zxp0002p5lo56189qbh", name: "Anna O", score: 18.5 },
+          ],
+          loser: [
+            {
+              id: "cmc5n3zxp0005p5loaf1n99a9",
+              name: "Rachelle J",
+              score: 9.75,
+            },
+            { id: "cmc5n3zxp000ip5loedokujy3", name: "Ari S", score: 6.5 },
+          ],
+        },
+        {
+          winner: [
+            { id: "cmc5n3zxp0000p5loow0ddrlx", name: "Erik O", score: 28.25 },
+            { id: "cmc5n3zxp0002p5lo56189qbh", name: "Anna O", score: 18.5 },
+          ],
+          loser: [
+            { id: "cmc5n3zxp0008p5lo2vk2vee8", name: "Tommy B", score: 11 },
+            { id: "cmc5n3zxp0004p5lopldq6jnd", name: "Dan F", score: 10.5 },
+          ],
+        },
+        {
+          winner: [
+            { id: "cmc5n3zxp000gp5lo5fmsmk57", name: "Brickwall", score: 14.5 },
+            { id: "cmc5n3zxp0003p5lo6s66t7yk", name: "Michelle C", score: 23 },
+          ],
+          loser: [
+            { id: "cmc5n3zxp0001p5lob5eewb2q", name: "Ofir C", score: 31.25 },
+            { id: "cmc5n3zxp000fp5lon3fv26zk", name: "Andraya B", score: 17.5 },
+          ],
+        },
+        {
+          winner: [
+            { id: "cmc5n3zxp0001p5lob5eewb2q", name: "Ofir C", score: 31.25 },
+            { id: "cmc5n3zxp000fp5lon3fv26zk", name: "Andraya B", score: 17.5 },
+          ],
+          loser: [
+            { id: "cmc5n3zxp0000p5loow0ddrlx", name: "Erik O", score: 28.25 },
+            { id: "cmc5n3zxp0002p5lo56189qbh", name: "Anna O", score: 18.5 },
+          ],
+        },
+        {
+          winner: [
+            { id: "cmc5n3zxp000gp5lo5fmsmk57", name: "Brickwall", score: 14.5 },
+            { id: "cmc5n3zxp0003p5lo6s66t7yk", name: "Michelle C", score: 23 },
+          ],
+          loser: [
+            { id: "cmc5n3zxp0001p5lob5eewb2q", name: "Ofir C", score: 31.25 },
+            { id: "cmc5n3zxp000fp5lon3fv26zk", name: "Andraya B", score: 17.5 },
+          ],
+        },
+        { winner: null, loser: null },
+        { winner: null, loser: null },
+      ],
+      date: "2024-05-18T00:00:00.000Z",
+    },
+  ];
+
+  await prisma.tournament.createMany({
+    data: tournaments.map((t) => ({
+      teams: t.teams,
+      results: t.results,
+      createdAt: new Date(t.date),
+    })),
+  });
+
+  console.log(`🌱 Seeded ${tournaments.length} tournaments`);
 }
 
 main()
